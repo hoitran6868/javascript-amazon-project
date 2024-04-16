e2789415b9de0ce2efe9d6cf3137c19128fea9a7
TranHoi <tranhoi.hust@gmail.com>
2024-04-16 21:59:13 +0900

    Delete code related 'Added' message and Move addToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,23 @@
 export const cart = [];
+
+export function addToCart(productId) {
+    let matchingItem;
+
+    cart.forEach((cartItem) => {
+        //Check if the product is already in the cart
+        if (productId === cartItem.productId) {
+            matchingItem = cartItem;
+        }
+    });
+
+    //If it is in the cart,increase the quantity
+    if (matchingItem) {
+        matchingItem.quantity += 1;
+    } else {
+        //If it is not in the cart,add it to the cart
+        cart.push({
+            productId: productId,
+            quantity: 1,
+        });
+    }
+}
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart, addToCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 
 let productsHTML = "";
@@ -63,80 +63,20 @@ products.forEach((product) => {
 
 document.querySelector(".js-product-grid").innerHTML = productsHTML;
 
-// We're going to use an object to save the timeout ids.
-// The reason we use an object is because each product
-// will have its own timeoutId. So an object lets us
-// save multiple timeout ids for different products.
-// For example:
-// {
-//   'product-id1': 2,
-//   'product-id2': 5,
-//   ...
-// }
-// (2 and 5 are ids that are returned when we call setTimeout).
-const addedMessageTimeouts = {};
+function updateCartQuantity() {
+    let cartQuantity = 0;
+
+    cart.forEach((cartItem) => {
+        cartQuantity += cartItem.quantity;
+    });
+
+    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+}
 
 document.querySelectorAll(".js-add-to-cart").forEach((button) => {
     button.addEventListener("click", () => {
-        const { productId } = button.dataset;
-
-        let matchingItem;
-
-        cart.forEach((item) => {
-            //Check if the product is already in the cart
-            if (productId === item.productId) {
-                matchingItem = item;
-            }
-        });
-
-        const quantitySelector = document.querySelector(
-            `.js-quantity-selector-${productId}`
-        );
-
-        const quantity = Number(quantitySelector.value);
-
-        //If it is in the cart,increase the quantity
-        if (matchingItem) {
-            matchingItem.quantity += quantity;
-        } else {
-            //If it is not in the cart,add it to the cart
-            cart.push({
-                productId,
-                quantity,
-            });
-        }
-
-        let cartQuantity = 0;
-
-        cart.forEach((item) => {
-            cartQuantity += item.quantity;
-        });
-
-        document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
-
-        const addedMessage = document.querySelector(
-            `.js-added-to-cart-${productId}`
-        );
-
-        addedMessage.classList.add("added-to-cart-visible");
-
-        // Check if there's a previous timeout for this
-        // product. If there is, we should stop it.
-        const previousTimeoutId = addedMessageTimeouts[productId];
-        if (previousTimeoutId) {
-            clearTimeout(previousTimeoutId);
-        }
-
-        const timeoutId = setTimeout(() => {
-            addedMessage.classList.remove("added-to-cart-visible");
-        }, 2000);
-
-        // Save the timeoutId for this product
-        // so we can stop it later if we need to.
-        addedMessageTimeouts[productId] = timeoutId;
+        const productId = button.dataset.productId;
+        addToCart(productId);
+        updateCartQuantity();
     });
 });
-
-/*More advanced solution that uses a closure:
-https://github.com/SuperSimpleDev/javascript-course/pull/15/files
-*/
