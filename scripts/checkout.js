49088ab379b0717d6afc58ed0374c01fc179908d
TranHoi <tranhoi.hust@gmail.com>
2024-04-17 20:15:33 +0900

    Fix price formatting and radio selectors

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import { cart } from "../data/cart.js";
 import { products } from "../data/products.js";
+import { formatCurrency } from "./utils/money.js";
 
 let cartSummaryHTML = "";
 
@@ -30,9 +31,9 @@ cart.forEach((cartItem) => {
                     <div class="product-name">
                         ${matchingProduct.image}
                     </div>
-                    <div class="product-price">$${
-                        matchingProduct.priceCents / 100
-                    }</div>
+                    <div class="product-price">
+                        $${formatCurrency(matchingProduct.priceCents)}
+                    </div>
                     <div class="product-quantity">
                         <span>
                             Quantity:
@@ -62,7 +63,7 @@ cart.forEach((cartItem) => {
                             type="radio"
                             checked
                             class="delivery-option-input"
-                            name="delivery-option-1"
+                            name="delivery-option-${matchingProduct.id}"
                         />
                         <div>
                             <div class="delivery-option-date">
@@ -77,7 +78,7 @@ cart.forEach((cartItem) => {
                         <input
                             type="radio"
                             class="delivery-option-input"
-                            name="delivery-option-1"
+                            name="delivery-option-${matchingProduct.id}"
                         />
                         <div>
                             <div class="delivery-option-date">
@@ -92,7 +93,7 @@ cart.forEach((cartItem) => {
                         <input
                             type="radio"
                             class="delivery-option-input"
-                            name="delivery-option-1"
+                            name="delivery-option-${matchingProduct.id}"
                         />
                         <div>
                             <div class="delivery-option-date">
