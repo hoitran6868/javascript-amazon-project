0796126631c0ca31453486a5cd80e46f7acd3024
TranHoi <tranhoi.hust@gmail.com>
2024-05-12 15:07:56 +0900

    Load products from the backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,10 @@
 import { renderOrderSummary } from "./chekout/orderSummary.js";
 import { renderPaymentSummary } from "./chekout/paymentSummary.js";
+import { loadProducts } from "../data/products.js";
 //import "../data/cart-class.js";
-import "../data/backend-practice.js";
+//import "../data/backend-practice.js";
 
-renderOrderSummary();
-renderPaymentSummary();
+loadProducts(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+});
